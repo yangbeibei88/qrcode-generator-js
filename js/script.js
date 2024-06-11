19193dc5b3b9ba044c07e228d01fc5671f8500fe
BeibeiYang <chrisyangber@hotmail.com>
2024-06-11 15:59:14 +1000

    javascript form, qrcode

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>QR Code Generateor</title>
-</head>
-<body>
-  
-</body>
-</html>
+const form = document.getElementById("generate-qr-form");
+const qr = document.getElementById("qrcode");
+
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+
+  const url = document.getElementById("urlInput").value;
+  const size = document.getElementById("size").value;
+
+  console.log(url, size);
+
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    // after submit, display spinner first then disappear it in 1 sec
+    showSpinner();
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+    }, 1000);
+  }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode(qr, {
+    text: url,
+    width: size,
+    height: size,
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+};
+
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
+};
+
+const clearUI = () => {
+  qr.innerHTML = "";
+};
+
+form.addEventListener("submit", onGenerateSubmit);
